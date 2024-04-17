fe1a7a4dd984e97064ce0788f3b8e48256c21e74
akhlaque <akhlaque.ak@gmail.com>
2024-04-17 12:52:18 -0400

    updating

--- kplex-graph.h
+++ kplex-graph.h
@@ -95,12 +95,12 @@ public:
 
         printf("*** Degeneracy k-plex size: %u, max_core: %u, ub: %u, Time: %lu (microseconds)\n", V - idx, max_core, ub, t.elapsed());
 
-        // if (V - idx > kplex.size())
-        // {
-        //     kplex.clear();
-        //     for (ui i = idx; i < V; i++)
-        //         kplex.push_back(peelSeq[i]);
-        // }
+        if (V - idx > kplex.size())
+        {
+            kplex.clear();
+            for (ui i = idx; i < V; i++)
+                kplex.push_back(peelSeq[i]);
+        }
         return ub;
     }
 
