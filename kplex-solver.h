0462e0c6f181fd6a2b6d99e342b2df416955625d
akhlaque <akhlaque.ak@gmail.com>
2024-04-19 16:35:31 -0400

    updating

--- kplex-solver.h
+++ kplex-solver.h
@@ -1054,7 +1054,7 @@ public:
         for (ui i = st + 1; i < en; i++)
         {
             ui v = C.get(i);
-            if (peelOrder[v] > peelOrder[bn])
+            if (peelOrder[SR[v]] > peelOrder[SR[bn]])
                 // if(g.adjList[v].size()>g.adjList[bn].size())
                 bn = v, ind = i;
         }
