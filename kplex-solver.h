42a151232a36b1242a91de28e8927bb733733ef3
akhlaque <akhlaque.ak@gmail.com>
2024-06-03 16:14:57 -0400

    updating

--- kplex-solver.h
+++ kplex-solver.h
@@ -746,6 +746,7 @@ public:
             ui u = C.fakeRecPop();
             for (ui v = 0; v < n; v++)
                 if (matrix[u * n + v])
+                if(v<sz1h or P.contains(v))
                     dG[v]++;
         }
     }
@@ -1246,9 +1247,9 @@ public:
             if (matrix[u * n + v])
                 if (v < sz1h or P.contains(v))
                     dG[v]++, dP[v]++;
-        // for (ui i = 0; i < P.size(); i++)
-        //     if (matrix[u * n + P[i]])
-        //         dG[u]++, dP[u]++;
+        for (ui i = 0; i < P.size(); i++)
+            if (matrix[u * n + P[i]])
+                dG[u]++, dP[u]++;
         return u;
     }
     ui removeFromP_K()
