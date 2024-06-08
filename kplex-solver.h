c98c5678a4f204dac5098acdb32768d824645039
akhlaque <akhlaque.ak@gmail.com>
2024-06-08 01:18:54 -0400

    updating

--- kplex-solver.h
+++ kplex-solver.h
@@ -150,37 +150,37 @@ public:
         ui max_core = 0, UB = 0, idx = n;
 
         // memset(vis, 0, sizeof(ui) * n);
-        bmp.reset(n);
-        for (ui i = 0; i < n; i++)
-        {
-            ui u, min_degree = n;
-            for (ui j = 0; j < n; j++)
-                if (!bmp.test(j) && degree[j] < min_degree)
-                {
-                    u = j;
-                    min_degree = degree[j];
-                }
-            if (min_degree > max_core)
-                max_core = min_degree;
-            core[u] = max_core;
-            peel_sequence[i] = u;
-            peelOrder[u] = i;
-            // vis[u] = 1;
-            bmp.set(u);
+        // bmp.reset(n);
+        // for (ui i = 0; i < n; i++)
+        // {
+        //     ui u, min_degree = n;
+        //     for (ui j = 0; j < n; j++)
+        //         if (!bmp.test(j) && degree[j] < min_degree)
+        //         {
+        //             u = j;
+        //             min_degree = degree[j];
+        //         }
+        //     if (min_degree > max_core)
+        //         max_core = min_degree;
+        //     core[u] = max_core;
+        //     peel_sequence[i] = u;
+        //     peelOrder[u] = i;
+        //     // vis[u] = 1;
+        //     bmp.set(u);
 
-            ui t_UB = core[u] + K;
-            if (n - i < t_UB)
-                t_UB = n - i;
-            if (t_UB > UB)
-                UB = t_UB;
+        //     ui t_UB = core[u] + K;
+        //     if (n - i < t_UB)
+        //         t_UB = n - i;
+        //     if (t_UB > UB)
+        //         UB = t_UB;
 
-            if (idx == n && min_degree + K >= n - i)
-                idx = i;
+        //     if (idx == n && min_degree + K >= n - i)
+        //         idx = i;
 
-            for (ui j = 0; j < n; j++)
-                if (!bmp.test(j) && matrix[u * n + j])
-                    --degree[j];
-        }
+        //     for (ui j = 0; j < n; j++)
+        //         if (!bmp.test(j) && matrix[u * n + j])
+        //             --degree[j];
+        // }
 
 
             // heap doesn't work better in several datasets e.g. soc-FourSquare
