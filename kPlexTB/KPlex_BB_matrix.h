dba039efceca22752de140be9771e9ea0bd4b5f2
akhlaque <akhlaque.ak@gmail.com>
2024-06-14 13:54:07 -0400

    updating

--- kPlexTB/KPlex_BB_matrix.h
+++ kPlexTB/KPlex_BB_matrix.h
@@ -559,7 +559,7 @@ private:
 			if(root_level) found_larger=false;
 			else
 			if(found_larger) continue;
-			ui pre_best_solution_size = best_solution_size, t_old_S_end = S_end, t_old_R_end = R_end, t_old_removed_edges_n = old_removed_edges_n;
+			ui pre_best_solution_size = best_solution_size, t_old_S_end = S_end, t_old_R_end = R_end, t_old_removed_edges_n = 0;
 			if(move_u_to_S_with_prune(u, S_end, R_end, level)) BB_search(S_end, R_end, level+1, false, false);
 			// if(best_solution_size >= _UB_) return ;
 			restore_SR_and_edges(S_end, R_end, t_old_S_end, t_old_R_end, level, t_old_removed_edges_n);			
