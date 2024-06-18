b6737ce52608409cc01f706fc3d73d2dbaf3839c
akhlaque <akhlaque.ak@gmail.com>
2024-06-18 02:24:57 -0400

    updating

--- kPlexTB/Graph.cpp
+++ kPlexTB/Graph.cpp
@@ -319,9 +319,8 @@ void Graph::kPlex_exact(int mode) {
 				reorganize_adjacency_lists(n, peel_sequence, rid, pstart, pend, edges);
 				ui sz1h = 0;
 
-				// for(ui i = n;i > 0&&kplex.size() < UB;i --) {
-				for(ui i = 0;i < n&&kplex.size() < UB;i ++) {
-					ui u = peel_sequence[i];
+				for(ui i = n;i > 0&&kplex.size() < UB;i --) {
+					ui u = peel_sequence[i-1];
 					// printf("solving %u ", u);
 					if(pend[u]-pstart[u]+K <= kplex.size()||n-i < kplex.size()) continue;
 
