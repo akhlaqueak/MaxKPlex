a4c13e524652955bbc654d7dd88de42b129d9b8d
akhlaque <akhlaque.ak@gmail.com>
2024-06-23 01:32:29 -0400

    updating

--- kPlexSBs-forward/main.cpp
+++ kPlexSBs-forward/main.cpp
@@ -621,7 +621,8 @@ ui Graph::extract_subgraph_and_prune(ui u, ui *ids, ui &ids_n, ui *rid, ui* p_ri
 	for(ept i = pstart[u];i < pend[u];i ++) if(!deleted[edgelist_pointer[i]]) {
 		edges[u_n] = edges[i]; edgelist_pointer[u_n++] = edgelist_pointer[i];
 		ui v = edges[i];
-		if(p_rid[u]<p_rid[v]){
+		// if(p_rid[u]<p_rid[v])
+		{
 			ids[ids_n++] = v; exists[v] = 2;
 		}
 	}
@@ -660,7 +661,9 @@ ui Graph::extract_subgraph_and_prune(ui u, ui *ids, ui &ids_n, ui *rid, ui* p_ri
 	for(ui i = 1;i < nr_size;i ++) if(exists[ids[i]] == 2) {
 		ui uu = ids[i];
 		for(ept j = pstart[uu];j < pend[uu];j ++) {
-			if(!exists[edges[j]] and p_rid[u]<p_rid[edges[j]]) {
+			if(!exists[edges[j]] 
+			// and p_rid[u]<p_rid[edges[j]]
+			) {
 				ids[ids_n++] = edges[j];
 				exists[edges[j]] = 3;
 				degree[edges[j]] = 1;
