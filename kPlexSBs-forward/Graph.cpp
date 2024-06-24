b9f3276e157ff018f5031f5f7f9f0287e814cbf7
akhlaque <akhlaque.ak@gmail.com>
2024-06-24 01:39:04 -0400

    updating

--- kPlexSBs-forward/Graph.cpp
+++ kPlexSBs-forward/Graph.cpp
@@ -393,7 +393,7 @@ void Graph::kPlex_exact() {
 		ui *p_rid = new ui[n];
 		for(ui i=0;i<n; i++)p_rid[peel_sequence[i]] = i;
 		ui UB_t = UB;
-#define FORWARD
+// #define FORWARD
 #ifdef FORWARD
 		for(int i = 0;i < n&&m&&kplex.size() < UB;i ++) {
 			ui u, key;
