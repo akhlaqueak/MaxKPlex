702058d1d497665cbcc6a7fd3672b93f6cc60501
akhlaque <akhlaque.ak@gmail.com>
2024-06-23 22:50:27 -0400

    updating

--- kPlexTB/Graph.cpp
+++ kPlexTB/Graph.cpp
@@ -321,7 +321,7 @@ void Graph::kPlex_exact(int mode) {
 				if(pend == nullptr) pend = new ept[n+1];
 				reorganize_adjacency_lists(n, peel_sequence, rid, pstart, pend, edges);
 				ui sz1h = 0;
-		// #define REVERSE
+#define REVERSE
 #ifdef REVERSE
 				for(ui i = 0;i < n&&kplex.size() < UB;i ++) {
 					ui u = peel_sequence[i];
