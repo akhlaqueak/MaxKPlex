f3b9de88b7a3fe57720d6447e462a4413c086616
akhlaque <akhlaque.ak@gmail.com>
2024-06-25 15:34:11 -0400

    updating

--- kPlexSBs-forward/KPlex_BB_matrix.h
+++ kPlexSBs-forward/KPlex_BB_matrix.h
@@ -516,7 +516,8 @@ private:
 		}
 		#ifdef SEESAW
 		seesaw.tick();
-		if (seesawUB(S_end, R_end)<=best_solution_size) {
+		ui comp = CSIZE*CSIZE*S_end;
+		if (comp < 1000 and seesawUB(S_end, R_end)<=best_solution_size) {
 			restore_SR_and_edges(S_end, R_end, old_S_end, old_R_end, level, old_removed_edges_n);
 			return ;
 		}
