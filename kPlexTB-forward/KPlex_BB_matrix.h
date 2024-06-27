89133f5b97f63d6f1f42fe072eec8e5cc7def53f
akhlaque <akhlaque.ak@gmail.com>
2024-06-27 17:53:08 -0400

    updating

--- kPlexTB-forward/KPlex_BB_matrix.h
+++ kPlexTB-forward/KPlex_BB_matrix.h
@@ -565,13 +565,13 @@ private:
 			}
 
 
+			if(best_solution_size >= _UB_) return ;
 			// if a larger kplex is found, branches will be generated only at root level
 			if(root_level) found_larger=false;
 			else
 			if(found_larger) continue;
 			ui pre_best_solution_size = best_solution_size, t_old_S_end = S_end, t_old_R_end = R_end, t_old_removed_edges_n = 0;
 			if(move_u_to_S_with_prune(u, S_end, R_end, level)) BB_search(S_end, R_end, level+1, false, false);
-			// if(best_solution_size >= _UB_) return ;
 			restore_SR_and_edges(S_end, R_end, t_old_S_end, t_old_R_end, level, t_old_removed_edges_n);			
 		}
 		// for(ui i=R_end-1;i>=S_end;i--){
@@ -1637,7 +1637,7 @@ private:
 		seesaw.tock();
         return UB;
     }
-	
+
     void createIS(ui S_end, ui R_end)
     {
         ISc.clear();
