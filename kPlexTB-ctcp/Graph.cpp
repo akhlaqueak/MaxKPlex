6d55e3b2b24782cd956fc63e052be90bd7372a2b
akhlaque <akhlaque.ak@gmail.com>
2024-06-28 18:06:06 -0400

    updating

--- kPlexTB-ctcp/Graph.cpp
+++ kPlexTB-ctcp/Graph.cpp
@@ -507,7 +507,7 @@ ui Graph::extract_subgraph_with_prune(ui u, ui degree_threshold, ui triangle_thr
 	}
 	assert(pend[u] >= pstart[u+1]||p_rid[edges[pend[u]]] < p_rid[u]);
 
-	print_array("ids1", rid, 0, ids.size(), 0);
+	print_array("ids1", ids.data(), 0, ids.size(), 0);
 	// Utility::print_array("ids1", ids.data(), 0, ids.size(), 0);
 
 	ui *Q = rid;
