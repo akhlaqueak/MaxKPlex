b37d0851c73d7f93da7164295ffe4099b4de1b97
akhlaque <akhlaque.ak@gmail.com>
2024-06-28 23:27:14 -0400

    updating

--- kPlexTB-ctcp/Graph.cpp
+++ kPlexTB-ctcp/Graph.cpp
@@ -538,7 +538,7 @@ ui Graph::extract_subgraph_with_prune(ui u, ui degree_threshold, ui triangle_thr
 	for(ui i = 1;i < old_size;i ++) if(exists[ids[i]] == 2) {
 		ui v = ids[i];
 		for(ept j = pstart[v];j < pend[v];j ++) {
-			if(deleted[edgelist_pointer[j]] or p_rid[edges[j]] < p_rid[u] or exists[edges[j]]==3)
+			if(deleted[edgelist_pointer[j]] or p_rid[edges[j]] < p_rid[u] )
 				continue;
 			if(!exists[edges[j]]) {
 				ids.push_back(edges[j]);
@@ -567,8 +567,7 @@ ui Graph::extract_subgraph_with_prune(ui u, ui degree_threshold, ui triangle_thr
 
 	for(ui i = 0;i < ids.size();i ++) {
 		ui v = ids[i];
-		for(ept j = pstart[v];j < pend[v];j ++) if(exists[edges[j]]
-		and v>edges[j] and p_rid[u]<p_rid[edges[j]]) {
+		for(ept j = pstart[v];j < pend[v];j ++) if(exists[edges[j]] && v>edges[j]) {
 			// if(rid[v] < ids.size()&&rid[edges[j]] < ids.size()&&v<edges[j])
 			vp.push_back(make_pair(rid[v], rid[edges[j]]));
 			// else
