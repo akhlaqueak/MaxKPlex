7f76e6d5acf077d2412a000147b726c3a876135b
akhlaque <akhlaque.ak@gmail.com>
2024-06-30 02:28:33 -0400

    updating

--- kPlexTB-ctcp/Graph.cpp
+++ kPlexTB-ctcp/Graph.cpp
@@ -435,6 +435,7 @@ void Graph::kPlex_exact(int mode) {
 
 					if(dual_mode and dual_mode_timer.elapsed()/1'000'000 > dual_mode_thresh ){
 						forward = !forward; // dual solutions
+						dual_mode_timer.restart();
 						if(forward)
 						cout<<"Direction changed, L->R"<<endl;
 						else
