39aa4ad5f16b6ccc0106b0f8c9631b0122bca58c
akhlaque <akhlaque.ak@gmail.com>
2024-07-01 16:49:58 -0400

    updating

--- kPlexTB/Graph.cpp
+++ kPlexTB/Graph.cpp
@@ -347,14 +347,14 @@ void Graph::kPlex_exact(int mode) {
 					if(density < min_density) min_density = density;
 
 					ui t_old_size = kplex.size();
-						kplex_solver_m->load_graph(ids.size(), vp, sz1h);
-						kplex_solver_m->kPlex(K, UB_t, kplex, true);
+						// kplex_solver_m->load_graph(ids.size(), vp, sz1h);
+						// kplex_solver_m->kPlex(K, UB_t, kplex, true);
 					if(kplex.size() > t_old_size) {
 						printf("Larger kplex found at %u", u);
 						for(ui j = 0;j < kplex.size();j ++) kplex[j] = ids[kplex[j]];
 					}
 					// printf("solving %u \n", i);
-					printf(" total_elapased: %llu iteration: %u u: %u \tt_seesaw: %f \tt_2_hop_reduction: %f \tt_branchings %f\n", tt.elapsed(), i, u, seesaw.ticktock(), reductions.ticktock(), branchings.ticktock());
+					printf(" density: %llu iteration: %u u: %u \tt_seesaw: %f \tt_2_hop_reduction: %f \tt_branchings %f\n", tt.elapsed(), i, u, seesaw.ticktock(), reductions.ticktock(), branchings.ticktock());
 				}
 				delete kplex_solver_m;
 
