1df2781a65c8131ccbd8299ef49c169918b98468
akhlaque <akhlaque.ak@gmail.com>
2024-07-01 18:28:28 -0400

    updating

--- kPlexTB-hybrid/Graph.cpp
+++ kPlexTB-hybrid/Graph.cpp
@@ -350,7 +350,7 @@ void Graph::kPlex_exact(int mode) {
 					if(density < min_density) min_density = density;
 
 					ui t_old_size = kplex.size();
-					if(density>0.5){
+					if(density>0.6){
 						kp_solver->load(ids.size(), vp);
 						kp_solver->run(K, kplex);
 					}else{
