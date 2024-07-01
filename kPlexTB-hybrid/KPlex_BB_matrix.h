47a1437d478c0067ff10a0c1afabb56092a50b1e
akhlaque <akhlaque.ak@gmail.com>
2024-07-01 18:03:07 -0400

    updating

--- kPlexTB-hybrid/KPlex_BB_matrix.h
+++ kPlexTB-hybrid/KPlex_BB_matrix.h
@@ -206,6 +206,7 @@ public:
 		best_solution_size = kplex.size();
 		ui R_end;
 		initialization(R_end, must_include_0);
+		if(seesawUB(0, R_end)<=_UB_){cout<<"Failed... "; return;}
 		if(R_end&&best_solution_size < _UB_) BB_search(0, R_end, 1, must_include_0, true);
 		if(best_solution_size > kplex.size()) {
 			kplex.clear();
