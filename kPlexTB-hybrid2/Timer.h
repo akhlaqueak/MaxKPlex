385253aa250e22cdcfec7214ca334e2425ea0462
akhlaque <akhlaque.ak@gmail.com>
2024-07-10 00:44:37 -0400

    updating

---
+++ kPlexTB-hybrid2/Timer.h
@@ -0,0 +1,40 @@
+/******************************************************************************
+ * timer.h 
+ * record the elapsed time in microseconds (10^{-6} second)
+ *****************************************************************************/
+
+#ifndef _TIMER_LJ_
+#define _TIMER_LJ_
+
+#include <cstdlib>
+#include <sys/time.h>
+#include<chrono>
+using namespace std::chrono;
+
+class Timer
+{
+#define TIME_NOW std::chrono::steady_clock::now()
+
+public:
+	Timer() : m_start(TIME_NOW) {}
+	void restart() { m_start = TIME_NOW; }
+	long long elapsed()
+	{
+		return std::chrono::duration_cast<std::chrono::microseconds>(TIME_NOW - m_start).count();
+	}
+	void tick(){
+		tic = TIME_NOW;
+	}
+	void tock(){
+		toc+=std::chrono::duration_cast<std::chrono::nanoseconds>(TIME_NOW - tic).count();
+	}
+	double ticktock(){
+		return toc/(1000'000'000.0);
+	}
+private:
+	std::chrono::steady_clock::time_point m_start, tic;
+	unsigned long long toc=0;
+};
+
+
+#endif
