8773920aeb35c303eb24740655d9f2cb0a826e13
akhlaque <akhlaque.ak@gmail.com>
2024-07-16 15:09:44 -0400

    updating

--- kPlexTB-hybrid2/Graph.cpp
+++ kPlexTB-hybrid2/Graph.cpp
@@ -337,7 +337,6 @@ void Graph::kPlex_exact(int mode) {
 						// printf("solving by kPlexTB ");
 						if(pend[u]-pstart[u]+K <= kplex.size()||n-i < kplex.size()) continue;
 
-						fflush(stdout);
 
 						if(kplex.size() >= 2*K-1) sz1h = extract_subgraph_with_prune(u, kplex.size()+1-K, kplex.size()+1-2*K, kplex.size()+3-2*K, peel_sequence_rid, degree, ids, rid, vp, vis, pstart, pend, edges);
 						else sz1h = extract_subgraph_wo_prune(u, peel_sequence_rid, ids, rid, vp, vis, pstart, pend, edges);
@@ -353,6 +352,7 @@ void Graph::kPlex_exact(int mode) {
 						// dbdd_instance=true;
 					}
 
+						fflush(stdout);
 
 					if(ids.empty()||ids.size() <= kplex.size()) continue;
 
