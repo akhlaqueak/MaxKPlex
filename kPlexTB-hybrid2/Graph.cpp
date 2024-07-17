45cd2eeff99a0c96502772b8698c62a1fb224f9f
akhlaque <akhlaque.ak@gmail.com>
2024-07-16 23:38:13 -0400

    updating

--- kPlexTB-hybrid2/Graph.cpp
+++ kPlexTB-hybrid2/Graph.cpp
@@ -383,7 +383,7 @@ void Graph::kPlex_exact(int mode) {
 					}
 					// printf("%f\n", iteration.elapsed()/1000.0);
 					// printf("density %f ", density);
-					// printf(" total_elapased: %llu iteration: %u u: %u \tt_seesaw: %f \tt_2_hop_reduction: %f \tt_branchings %f\n", tt.elapsed(), i, u, seesaw.ticktock(), reductions.ticktock(), branchings.ticktock());
+					printf(" total_elapased: %llu iteration: %u u: %u \tt_seesaw: %f \tt_2_hop_reduction: %f \tt_branchings %f\n", tt.elapsed(), i, u, seesaw.ticktock(), reductions.ticktock(), branchings.ticktock());
 				}
 				delete kplex_solver_m;
 
