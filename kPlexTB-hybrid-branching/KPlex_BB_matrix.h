52839ed1ae27eb99d6178334d823d6dcbf0f8e5f
akhlaque <akhlaque.ak@gmail.com>
2024-07-29 15:26:27 -0400

    updating

--- kPlexTB-hybrid-branching/KPlex_BB_matrix.h
+++ kPlexTB-hybrid-branching/KPlex_BB_matrix.h
@@ -621,7 +621,7 @@ private:
         // the second branch exclude u from G	
 		{
 			while(!Qv.empty()) Qv.pop();
-			Qv.push(pivot);
+			Qv.push(u);
 			ui pre_best_solution_size = best_solution_size, t_old_S_end = S_end, t_old_R_end = R_end, t_old_removed_edges_n = 0;
 			if(remove_vertices_and_edges_with_prune(S_end, R_end, level)) BB_search(S_end, R_end, level+1, false, false, endIdx, endIdx);
 			restore_SR_and_edges(S_end, R_end, t_old_S_end, t_old_R_end, level, t_old_removed_edges_n);	
