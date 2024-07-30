0a471530126981e8331d37bfd35a08dff260e2bd
akhlaque <akhlaque.ak@gmail.com>
2024-07-30 16:00:28 -0400

    updating

--- kPlexTB-hybrid-branching/Graph.cpp
+++ kPlexTB-hybrid-branching/Graph.cpp
@@ -358,7 +358,7 @@ void Graph::kPlex_exact(int mode) {
 				// reorganize_adjacency_lists(n, peel_sequence, rid, pstart, pend, edges);
 				ui sz1h = 0;
 				ui UB_t = UB;
-				bool forward=K>=5, dual_mode = false;
+				bool forward=false, dual_mode = false;
 				Timer dual_mode_timer;
 				ui dual_mode_thresh = 10, p_left = 0, p_right = n;
 				if(forward)
--- kPlexTB-hybrid-branching/KPlex_BB_matrix.h
+++ kPlexTB-hybrid-branching/KPlex_BB_matrix.h
@@ -554,7 +554,7 @@ private:
 #endif
 
 
-if(K<=5){
+if(true){
 
 // ******************* Adding our branching stuff here... 
 		ui t_R_end=R_end;
