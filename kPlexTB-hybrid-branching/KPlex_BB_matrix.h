a0b6df448ad22421838393b9ff22aeef94c3a59a
akhlaque <akhlaque.ak@gmail.com>
2024-07-30 16:12:47 -0400

    updating

--- kPlexTB-hybrid-branching/KPlex_BB_matrix.h
+++ kPlexTB-hybrid-branching/KPlex_BB_matrix.h
@@ -167,7 +167,7 @@ public:
 		n = _n;
 		sz1h = _sz1h;
 		sparse=vp.size()*2/(double)n/(n-1) < 0.9;
-		ctcp_enabled=K>5;
+		ctcp_enabled=false;
 		if(((long long)n)*n > matrix_size) {
 			do {
 				matrix_size *= 2;
