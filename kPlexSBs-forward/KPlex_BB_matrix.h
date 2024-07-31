134eda9b657783cf87bc636600171fbae4f6684e
akhlaque <akhlaque.ak@gmail.com>
2024-07-31 14:23:33 -0400

    updating

--- kPlexSBs-forward/KPlex_BB_matrix.h
+++ kPlexSBs-forward/KPlex_BB_matrix.h
@@ -416,8 +416,8 @@ private:
 
 	void BB_search(ui S_end, ui R_end, ui level, bool choose_zero, bool root_level, ui begIdx=0, ui endIdx=0) {
 		if(S_end > best_solution_size) store_solution(S_end);
-		if(R_end > best_solution_size&&is_kplex(R_end)) store_solution(R_end);
-		if(R_end <= best_solution_size+1 || best_solution_size >= _UB_) return ;
+		// if(R_end > best_solution_size&&is_kplex(R_end)) store_solution(R_end);
+		// if(R_end <= best_solution_size+1 || best_solution_size >= _UB_) return ;
 
 #ifndef NDEBUG
 		for(ui i = 0;i < R_end;i ++) {
