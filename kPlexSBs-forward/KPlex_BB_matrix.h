fbf04fa9038a7ecae95b99b10f58d36b7b82d0b7
akhlaque <akhlaque.ak@gmail.com>
2024-07-31 14:50:01 -0400

    updating

--- kPlexSBs-forward/KPlex_BB_matrix.h
+++ kPlexSBs-forward/KPlex_BB_matrix.h
@@ -405,7 +405,7 @@ private:
 		best_solution_size = size;
 		for(ui i = 0;i < best_solution_size;i ++) best_solution[i] = SR[i];
 		for(ui i = 0;verify&&i < best_solution_size;i ++) {
-			if(degree_in_S[SR[i]]+K<best_solution_size) cout<<degree_in_S[SR[i]]<<" ";
+			if(degree_in_S[SR[i]]+K<best_solution_size) cout<<degree_in_S[SR[i]]<<" Invalid ";
 		}
 	}
 
@@ -557,7 +557,7 @@ private:
 #endif
 
 
-if(false){
+if(true){
 
 // ******************* Adding our branching stuff here... 
 		ui t_R_end=R_end;
