c91d28730ab42846b9da06cc577da080f0ab22fd
akhlaque <akhlaque.ak@gmail.com>
2024-08-01 22:22:01 -0400

    updating

--- Maple-kPlexT/MSearcher.h
+++ Maple-kPlexT/MSearcher.h
@@ -169,7 +169,7 @@ public:
 	void load_graph(ui _n, const std::vector<std::pair<ui,ui> > &vp) {
 		n = _n;
 		sparse=vp.size()*2/(double)n/(n-1) < 0.9;
-		ctcp_enabled=(K>=INNER_CTCP);
+		ctcp_enabled=false;//(K>=INNER_CTCP);
 		if(((long long)n)*n > matrix_size) {
 			do {
 				matrix_size *= 2;
