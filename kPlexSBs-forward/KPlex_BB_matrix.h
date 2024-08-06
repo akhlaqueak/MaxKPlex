d83412331b4b80485e519064f4152a9809ce91f6
akhlaque <akhlaque.ak@gmail.com>
2024-08-06 14:29:40 -0400

    updating

--- kPlexSBs-forward/KPlex_BB_matrix.h
+++ kPlexSBs-forward/KPlex_BB_matrix.h
@@ -1097,101 +1097,102 @@ else{
 	// 	branchings.tock();
     ui getBranchings(ui S_end, ui R_end, ui level)
     {
-        for (ui i = 0; i < S_end; i++)
-        {
-            ui u = SR[i];
-            psz[i] = 0;
-            if (support(S_end, u) == 0)
-                continue;
-            // skipping it, because this is a boundary vertex, and it can't have any non-neighbor candidate
-            // Lookup neig(&lookup, &g.adjList[u]);
-            // bmp.setup(g.adjList[u], g.V);
-			ui* t_LPI = LPI+i*n;
-			char* t_matrix=matrix+u*n;
-            for (ui j = S_end; j < R_end; j++)
-            {
-                ui v = SR[j];
-                if (!t_matrix[v])
-                    // PI[u].push_back(v);
-                    t_LPI[psz[i]++] = v;
-            }
-        }
-        ui beta = best_solution_size - S_end;
-        ui cend = S_end;
-		branchings.tick();
-        while (true)
-        {
-            ui maxpi = -1;
-            double maxdise = 0;
-            for (ui i = 0; i < S_end; i++)
-            {
-                ui u = SR[i];
-                if (psz[i] == 0)
-                    continue;
-                double cost = min(support(S_end, u), psz[i]);
-                double dise = psz[i] / cost;
-                if (cost <= beta and dise > maxdise)
-                    maxpi = i, maxdise = dise;
-            }
-            if (maxpi != -1)
-            {
-                bmp.reset(n);
-                for (ui i = 0; i < psz[maxpi]; i++)
-                    bmp.set(LPI[maxpi * n + i]);
-                // remove pi* from C
-                for (ui i = cend; i < R_end; i++)
-                {
-                    ui v = SR[i];
-                    if (bmp.test(v))
-                    {
-                        // rather than removing from C, we are changing the positions within C.
-                        // When function completes
-                        // [0...cend) holds all vertices C\B, and [cend, sz) holds the B.
-                        swap_pos(cend++, i);
-                    }
-                }
-                // beta-=cost(pi*)
-                beta -= min(support(S_end, SR[maxpi]), psz[maxpi]);
-                // remove maxpi from every pi
-                for (ui i = 0; i < S_end; i++)
-                {
-                    // Removing pi* from all pi in PI
-                    if (i == maxpi or psz[i]==0)
-                        continue;
-                    ui u = SR[i];
-                    ui j = 0;
-					ui* t_LPI = LPI+i*n;
-                    for (ui k = 0; k < psz[i]; k++)
-                        if (!bmp.test(t_LPI[k]))
-                            // if (!bmp.test(PI[u][k]))
-                            // PI[u][j++] = PI[u][k];
-							t_LPI[j++] = t_LPI[k];
-                            // LPI[u * n + j++] = LPI[u * n + k];
-                    // PI[u].resize(j);
-                    psz[i] = j;
-                }
-                // remove maxpi...
-                // PI[maxpi].clear();
-                psz[maxpi] = 0;
-            }
-            else
-                break;
-            if (beta == 0)
-                break;
-        }
-		branchings.tock();
-        if (beta > 0)
-            cend += min(beta, R_end - cend);
+		ui cend=bound(S_end, R_end);
+        // for (ui i = 0; i < S_end; i++)
+        // {
+        //     ui u = SR[i];
+        //     psz[i] = 0;
+        //     if (support(S_end, u) == 0)
+        //         continue;
+        //     // skipping it, because this is a boundary vertex, and it can't have any non-neighbor candidate
+        //     // Lookup neig(&lookup, &g.adjList[u]);
+        //     // bmp.setup(g.adjList[u], g.V);
+		// 	ui* t_LPI = LPI+i*n;
+		// 	char* t_matrix=matrix+u*n;
+        //     for (ui j = S_end; j < R_end; j++)
+        //     {
+        //         ui v = SR[j];
+        //         if (!t_matrix[v])
+        //             // PI[u].push_back(v);
+        //             t_LPI[psz[i]++] = v;
+        //     }
+        // }
+        // ui beta = best_solution_size - S_end;
+        // ui cend = S_end;
+		// branchings.tick();
+        // while (true)
+        // {
+        //     ui maxpi = -1;
+        //     double maxdise = 0;
+        //     for (ui i = 0; i < S_end; i++)
+        //     {
+        //         ui u = SR[i];
+        //         if (psz[i] == 0)
+        //             continue;
+        //         double cost = min(support(S_end, u), psz[i]);
+        //         double dise = psz[i] / cost;
+        //         if (cost <= beta and dise > maxdise)
+        //             maxpi = i, maxdise = dise;
+        //     }
+        //     if (maxpi != -1)
+        //     {
+        //         bmp.reset(n);
+        //         for (ui i = 0; i < psz[maxpi]; i++)
+        //             bmp.set(LPI[maxpi * n + i]);
+        //         // remove pi* from C
+        //         for (ui i = cend; i < R_end; i++)
+        //         {
+        //             ui v = SR[i];
+        //             if (bmp.test(v))
+        //             {
+        //                 // rather than removing from C, we are changing the positions within C.
+        //                 // When function completes
+        //                 // [0...cend) holds all vertices C\B, and [cend, sz) holds the B.
+        //                 swap_pos(cend++, i);
+        //             }
+        //         }
+        //         // beta-=cost(pi*)
+        //         beta -= min(support(S_end, SR[maxpi]), psz[maxpi]);
+        //         // remove maxpi from every pi
+        //         for (ui i = 0; i < S_end; i++)
+        //         {
+        //             // Removing pi* from all pi in PI
+        //             if (i == maxpi or psz[i]==0)
+        //                 continue;
+        //             ui u = SR[i];
+        //             ui j = 0;
+		// 			ui* t_LPI = LPI+i*n;
+        //             for (ui k = 0; k < psz[i]; k++)
+        //                 if (!bmp.test(t_LPI[k]))
+        //                     // if (!bmp.test(PI[u][k]))
+        //                     // PI[u][j++] = PI[u][k];
+		// 					t_LPI[j++] = t_LPI[k];
+        //                     // LPI[u * n + j++] = LPI[u * n + k];
+        //             // PI[u].resize(j);
+        //             psz[i] = j;
+        //         }
+        //         // remove maxpi...
+        //         // PI[maxpi].clear();
+        //         psz[maxpi] = 0;
+        //     }
+        //     else
+        //         break;
+        //     if (beta == 0)
+        //         break;
+        // }
+		// branchings.tock();
+        // if (beta > 0)
+        //     cend += min(beta, R_end - cend);
 
-            // vertices in [cend, R_end) range are Branching vertices
-			// sort the branching vertices in ascending order of peelOrder, and remove from C
-		/*
-		ui begIdx=addList.size();
-		addList.insert(addList.end(), SR+cend, SR+R_end);
-		ui endIdx = addList.size();
-		std::sort(addList.data()+begIdx,addList.data()+endIdx,[&](int a,int b){return peelOrder[a]>peelOrder[b];});
-		return R_end-cend;
-		*/
+        //     // vertices in [cend, R_end) range are Branching vertices
+		// 	// sort the branching vertices in ascending order of peelOrder, and remove from C
+		// /*
+		// ui begIdx=addList.size();
+		// addList.insert(addList.end(), SR+cend, SR+R_end);
+		// ui endIdx = addList.size();
+		// std::sort(addList.data()+begIdx,addList.data()+endIdx,[&](int a,int b){return peelOrder[a]>peelOrder[b];});
+		// return R_end-cend;
+		// */
 		
 		for(ui i=cend; i<R_end; i++){
 			// get a vertex with highest peelOrder at location i
@@ -1218,15 +1219,6 @@ else{
 				if(t_matrix[w]) -- degree[w];
 			}
 		}
-		// assert(R_end==cend);
-		// for(ui i = 0;i < R_end;i ++) {
-		// 	ui d1 = 0, d2 = 0;
-		// 	for(ui j = 0;j < S_end;j ++) if(matrix[SR[i]*n + SR[j]]) ++ d1;
-		// 	d2 = d1;
-		// 	for(ui j = S_end;j < R_end;j ++) if(matrix[SR[i]*n + SR[j]]) ++ d2;
-		// 	assert(d1 == degree_in_S[SR[i]]);
-		// 	assert(d2 == degree[SR[i]]);
-		// }
         return cend;
     }
 	void collect_removable_vertices_based_on_total_edges(ui S2_n, ui S_end, ui R_end, ui level) {
