5d70a1e387c91daf2dd22e059d941c7d293cc6af
akhlaque <akhlaque.ak@gmail.com>
2024-08-06 22:03:44 -0400

    updating

--- Maple-kPlexT/Utility.h
+++ Maple-kPlexT/Utility.h
@@ -24,7 +24,7 @@
 #endif
 
 using ui = unsigned int; // vertex type
-using ept = unsigned long; // edge pointer type; unsigned int can be used to process upto two billion undirected edges
+// using ui = unsigned int; // edge pointer type; unsigned int can be used to process upto two billion undirected edges
 
 #define pb push_back
 #define mp make_pair
