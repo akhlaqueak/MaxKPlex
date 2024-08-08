23295c8d71c49f1f0faeaf4de21b127702f09e4a
akhlaque <akhlaque.ak@gmail.com>
2024-08-07 22:55:26 -0400

    updating

--- Maple-kPlexT/Graph.cpp
+++ Maple-kPlexT/Graph.cpp
@@ -236,7 +236,9 @@ void Graph::search() {
 		
 		delete[] core; core = NULL;
 
-		if(n<=kplex.size())
+		if(n<=kplex.size()){
+			
+		}
 			printf(">>%s \tMaxKPlex_Size: %lu t_Total: %f t_Seesaw: %f\n", dir.substr(dir.find_last_of("/")+1).c_str(), kplex.size(), t.elapsed()/1000000.0, 0.0);
 		
 		ui *degree = new ui[n];
