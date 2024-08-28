4f4220325f36f7fb442fb51f5784a9d7d7e61063
akhlaque <akhlaque.ak@gmail.com>
2024-08-28 18:09:38 -0400

    updating

--- Maple-kPlexT/MSearcher.h
+++ Maple-kPlexT/MSearcher.h
@@ -470,16 +470,16 @@ private:
 			return ;
 		}
 
-		ui S2_n = 0;
-		for(ui i = 0;i < S_end;i ++) if(R_end - degree[SR[i]] > K) S2[S2_n++] = SR[i];
+		// ui S2_n = 0;
+		// for(ui i = 0;i < S_end;i ++) if(R_end - degree[SR[i]] > K) S2[S2_n++] = SR[i];
 
-		if(S2_n >= 2) {
-			collect_removable_vertices_based_on_total_edges(S2_n, S_end, R_end, level);
-			if(!remove_vertices_and_edges_with_prune(S_end, R_end, level)) {
-				restore_SR_and_edges(S_end, R_end, old_S_end, old_R_end, level, old_removed_edges_n);
-				return ;
-			}
-		}
+		// if(S2_n >= 2) {
+		// 	collect_removable_vertices_based_on_total_edges(S2_n, S_end, R_end, level);
+		// 	if(!remove_vertices_and_edges_with_prune(S_end, R_end, level)) {
+		// 		restore_SR_and_edges(S_end, R_end, old_S_end, old_R_end, level, old_removed_edges_n);
+		// 		return ;
+		// 	}
+		// }
 
 #ifndef NDEBUG
 		for(ui i = 0;i < R_end;i ++) {
@@ -540,10 +540,10 @@ private:
 		seesaw.tick();
 		// ui comp = S_end*S_end * CSIZE;
 		// if (comp < 1000 and seesawUB(S_end, R_end)<=best_solution_size) {
-		// if (CSIZE > beta*3  and seesawUB(S_end, R_end)<=best_solution_size) {
-		// 	restore_SR_and_edges(S_end, R_end, old_S_end, old_R_end, level, old_removed_edges_n);
-		// 	return ;
-		// }
+		if (CSIZE > beta*3  and seesawUB(S_end, R_end)<=best_solution_size) {
+			restore_SR_and_edges(S_end, R_end, old_S_end, old_R_end, level, old_removed_edges_n);
+			return ;
+		}
 		seesaw.tock();
 		#endif
 #ifndef NDEBUG
