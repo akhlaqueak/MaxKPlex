3cff5fabdacc98ad980616c04dfbd715818680de
akhlaque <akhlaque.ak@gmail.com>
2024-09-16 18:34:51 -0400

    updating

--- Maple-kPlexT/MSearcher.h
+++ Maple-kPlexT/MSearcher.h
@@ -17,7 +17,7 @@ double cfactor=1;
 #define PART_BRANCH (K<10&&sparse)
 // #define INNER_CTCP_COND !PART_BRANCH
 // #define PART_BRANCH (false)
-#define INNER_CTCP_COND true
+#define INNER_CTCP_COND false
 // #define B_BRANCHINGS
 // #define BINARY_BRANCHINGS
 Timer seesaw, reductions, branchings;
