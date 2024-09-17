280c88a95535a0abc6b2f0d260430c74526119dc
akhlaque <akhlaque.ak@gmail.com>
2024-09-16 21:05:31 -0400

    updating

--- Maple-kPlexT/MSearcher.h
+++ Maple-kPlexT/MSearcher.h
@@ -519,9 +519,8 @@ private:
 
 		#ifdef SEESAW
 		seesaw.tick();
-		// ui comp = S_end*S_end * CSIZE;
-		// if (comp < 1000 and seesawUB(S_end, R_end)<=best_solution_size) {
-		if (CSIZE>3*beta && seesawUB(S_end, R_end)<=best_solution_size) {
+		// if (CSIZE>3*beta && seesawUB(S_end, R_end)<=best_solution_size) {
+		if (seesawUB(S_end, R_end)<=best_solution_size) {
 			restore_SR_and_edges(S_end, R_end, old_S_end, old_R_end, level, old_removed_edges_n);
 			return ;
 		}
