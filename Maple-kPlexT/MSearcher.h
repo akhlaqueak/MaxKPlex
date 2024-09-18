8a8065bacbb44f4313518778c27e4fda7bb8e1ac
akhlaque <akhlaque.ak@gmail.com>
2024-09-18 15:50:41 -0400

    updating

--- Maple-kPlexT/MSearcher.h
+++ Maple-kPlexT/MSearcher.h
@@ -1037,8 +1037,8 @@ else{
 				++ j;
 			}
 			if(ub>best_solution_size){
-				ub=bound(S_end, R_end, v, ISc);
-				if(ub<=best_solution_size) cout<<"r ";
+				// ub=bound(S_end, R_end, v, ISc);
+				// if(ub<=best_solution_size) cout<<"r ";
 			}
 			if(ub <= best_solution_size) {
 				level_id[v] = level;
