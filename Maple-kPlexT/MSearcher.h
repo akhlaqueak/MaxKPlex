7076d9e444bbdbdc3ca34b5274cad1e5839a2907
akhlaque <akhlaque.ak@gmail.com>
2024-09-19 12:42:18 -0400

    updating

--- Maple-kPlexT/MSearcher.h
+++ Maple-kPlexT/MSearcher.h
@@ -1064,7 +1064,7 @@ else{
 				++ j;
 			}
 			if(ub>best_solution_size){
-				ub=bound(S_end, R_end, v, ISc);
+				// ub=bound(S_end, R_end, v, ISc);
 				// if(ub<=best_solution_size) cout<<"r ";
 			}
 			if(ub <= best_solution_size) {
