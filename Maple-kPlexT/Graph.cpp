dcaf0a7d59b98e7aef2dc39b0da5b50048ac0d56
akhlaque <akhlaque.ak@gmail.com>
2024-09-19 16:31:40 -0400

    updating

--- Maple-kPlexT/Graph.cpp
+++ Maple-kPlexT/Graph.cpp
@@ -409,7 +409,7 @@ void Graph::search() {
 	delete[] core;
 	delete[] peel_sequence;
 
-	printf(">>%s \tMaxKPlex_Size: %lu t_Total: %f t_Seesaw: %f\n", dir.substr(dir.find_last_of("/")+1).c_str(), kplex.size(), t.elapsed()/1000000.0, bounding.elapsed()/1000000);
+	printf(">>%s \tMaxKPlex_Size: %lu t_Total: %f t_Seesaw: %f\n", dir.substr(dir.find_last_of("/")+1).c_str(), kplex.size(), t.elapsed()/1e6, bounding.elapsed()/1e6);
 
 	// printf("\tMaxKPlex_Size: %lu t_Total: %f t_Seesaw: %f\n", kplex.size(), t.elapsed()/1000000.0, 0);
 	// printf("\tMaximum kPlex Size: %lu, Total Time: %s (microseconds)\n", kplex.size(), Utility::integer_to_string(t.elapsed()).c_str());
