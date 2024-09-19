5088aaa6f8aed4ff063daac81d4248dc096ed780
akhlaque <akhlaque.ak@gmail.com>
2024-09-19 15:28:37 -0400

    updating

--- Maple-kPlexT/KPlex_BB_matrix.h
+++ Maple-kPlexT/KPlex_BB_matrix.h
@@ -550,7 +550,7 @@ private:
 // 			if(remove_vertices_and_edges_with_prune(S_end, R_end, level)) BB_search(S_end, R_end, level+1, false);
 // 			restore_SR_and_edges(S_end, R_end, t_old_S_end, t_old_R_end, level, t_old_removed_edges_n);	
 // 		}
-// 		restore_SR_and_edges(S_end, R_end, old_S_end, old_R_end, level, old_removed_edges_n);
+		restore_SR_and_edges(S_end, R_end, old_S_end, old_R_end, level, old_removed_edges_n);
 }
 
 	}
