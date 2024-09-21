5e864c760efb58615804988ce2a1de4461dae4bd
akhlaque <akhlaque.ak@gmail.com>
2024-09-21 18:10:40 -0400

    updating

--- Ours-parallel/Graph.cpp
+++ Ours-parallel/Graph.cpp
@@ -326,7 +326,7 @@ void Graph::kPlex_exact(int mode) {
 				std::fill(exists, exists+n, 0);n=0;
 				// std::fill(degree, degree+n, 0);
 				// std::fill(rid, rid+n, 0);
-				cout<<exists<<endl;
+				cout<<degree<<endl;
 
 				KPLEX_BB_MATRIX *kplex_solver_m = new KPLEX_BB_MATRIX();
 				kplex_solver_m->allocateMemory(n);
