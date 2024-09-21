08e14f11a761bc7db147ddb081ce117b23d332f7
akhlaque <akhlaque.ak@gmail.com>
2024-09-21 00:22:32 -0400

    updating

--- Ours-parallel/Graph.cpp
+++ Ours-parallel/Graph.cpp
@@ -1292,63 +1292,30 @@ int main(int argc, char *argv[]) {
 #else
 	printf("**** kPlexT (Release) build at %s %s ***\n", __TIME__, __DATE__);
 #endif
-
-	bool output = false;
-	bool binary_input = true;
-	bool mode = false;
-	string alg;
-
-	OptionParser op("Allowed options");
-	auto help_option = op.add<Switch>("h", "help", "\'produce help message\'");
-	auto graph_option = op.add<Value<string>>("g", "graph", "\'path to input graph file\'");
-	auto alg_option = op.add<Value<string>>("a", "alg", "\'algorithm name\' (degen | exact | verify)", "exact", &alg);
-	auto k_option = op.add<Value<int>>("k", "k", "\'the value of k for k-plex\'");
-	auto c_option = op.add<Value<int>>("c", "c", "\'the value of c-factor\'");
-	op.add<Switch>("o", "output", "\'write the kplex to ./kplex.txt\'", &output);
-	op.add<Switch>("b", "binary", "\'read the input graph from binary files b_adj.bin and b_degree.bin\'", &binary_input);
-
-	op.parse(argc, argv);
-
-	if(help_option->is_set()||argc <= 1) {
-		cout << op << endl;
-		if(argc <= 1) {
-			print_usage();
-			return 0;
-		}
+#define LEN_LIMIT (1<<10)
+char filename[LEN_LIMIT];
+	printf("\n-----------------------------------------------------------------------------------------\n");
+	if (argc == 3) {
+		strncpy(filename, argv[1], LEN_LIMIT);
+		int k = atoi(argv[2]);
+		Graph *graph = new Graph(filename, k);
+		graph->read_graph_binary();
+		graph->kPlex_exact(mode);
+		if(output) graph->output_one_kplex();
+		delete graph;
+		// delete graph; // there are some bugs in releasing memory
 	}
-	if(!graph_option->is_set()) {
-		printf("!!! The argument -g is required! Exit !!!\n");
-		return 0;
+	else if (argc == 4) {
+		strncpy(filename, argv[1], LEN_LIMIT);
+		int k = atoi(argv[2]);
+		Graph *graph = new Graph(filename, k);
+		graph->read_graph_binary();
+		graph->kPlex_exact(mode);
+		// graph->kPlex_dense();
+		if(output) graph->output_one_kplex();
+		delete graph;
 	}
-	if(!k_option->is_set()) {
-		printf("!!! The argument -k is required! Exit !!!\n");
-		return 0;
-	}
-
-	// if(!c_option->is_set()) {
-	// 	printf("!!! The argument -c is not provided, using c=1!!!\n");
-	// 	cfactor = 1;
-	// }
-	// else
-	// 	cfactor = c_option->value();
-
-	Graph *graph = new Graph(graph_option->value().c_str(), k_option->value());
-	if(binary_input) graph->read_graph_binary();
-	else graph->read_graph();
-
-#ifndef NDEBUG
-	printf("\t*** Finished reading graph\n");
-#endif
-
-	if(strcmp(alg.c_str(), "degen") == 0) graph->kPlex_degen(); //degeneracy-based k-plex
-	else if(strcmp(alg.c_str(), "exact") == 0) graph->kPlex_exact(mode);
-	else if(strcmp(alg.c_str(), "verify") == 0) graph->verify_kplex();
-	else print_usage();
-
-	if(output) graph->output_one_kplex();
-
-	delete graph;
-
-	printf("\n");
+	else printf("[usage]: exe file k [dense]\n");
+	printf("-----------------------------------------------------------------------------------------\n\n");
 	return 0;
 }
