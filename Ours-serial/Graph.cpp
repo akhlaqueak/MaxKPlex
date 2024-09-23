d4863134963243f2e9bc8e4383b44a9a7c175447
akhlaque <akhlaque.ak@gmail.com>
2024-09-22 21:42:11 -0400

    updating

--- Ours-serial/Graph.cpp
+++ Ours-serial/Graph.cpp
@@ -221,12 +221,10 @@ void Graph::search() {
 		shrink_graph(n, m, peel_sequence, core, out_mapping, nullptr, rid, pstart, edges, true);
 
 	
-		if(kplex.size()+1 > 2*K) {
-			CTPrune::core_truss_copruning(n, m, kplex.size()+1-K, kplex.size()+1-2*K, peel_sequence, out_mapping, rid, pstart, edges, degree, true);
-		}
+		// if(kplex.size()+1 > 2*K) {
+		// 	CTPrune::core_truss_copruning(n, m, kplex.size()+1-K, kplex.size()+1-2*K, peel_sequence, out_mapping, rid, pstart, edges, degree, true);
+		// }
 		ego_degen(n, m, peel_sequence, pstart, edges, degree, rid, vis, heap, edgelist_pointer, true);
-
-		kplex.resize(49);
 
 		if(kplex.size() > old_size) {
 			old_size = kplex.size();
@@ -234,10 +232,10 @@ void Graph::search() {
 				assert(kplex[i] < n);
 				kplex[i] = out_mapping[kplex[i]];
 			}
-
-			if(kplex.size()+1 > 2*K) CTPrune::core_truss_copruning(n, m, kplex.size()+1-K, kplex.size()+1-2*K, peel_sequence, out_mapping, rid, pstart, edges, degree, true);
-			else shrink_graph(n, m, peel_sequence, core, out_mapping, nullptr, rid, pstart, edges, true);
 		}
+
+		if(kplex.size()+1 > 2*K) CTPrune::core_truss_copruning(n, m, kplex.size()+1-K, kplex.size()+1-2*K, peel_sequence, out_mapping, rid, pstart, edges, degree, true);
+		else shrink_graph(n, m, peel_sequence, core, out_mapping, nullptr, rid, pstart, edges, true);
 		
 		delete[] core; core = NULL;
 
