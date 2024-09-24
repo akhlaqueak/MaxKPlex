027cda45c3196830cc3adaa40d9312e2ad75e8a2
akhlaque <akhlaque.ak@gmail.com>
2024-09-24 14:17:01 -0400

    updating

--- Ours-parallel/KPlex_BB_matrix.h
+++ Ours-parallel/KPlex_BB_matrix.h
@@ -709,7 +709,6 @@ else{ // pivot based branching
 			vp[i-S_end].second = nn;
 		}
 		ui *cnt = neighbors;
-		cout<<max_nn<<" ";
 		for(ui i = 0;i <= max_nn;i ++) cnt[i] = 0;
 		for(ui i = 0;i < vp.size();i ++) ++ cnt[vp[i].second];
 		for(ui i = 0;i < max_nn;i ++) cnt[i+1] += cnt[i];
