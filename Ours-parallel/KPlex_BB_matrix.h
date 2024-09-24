49471250840b046dc70f083f3bbc4d901eb3e271
akhlaque <akhlaque.ak@gmail.com>
2024-09-24 18:29:40 -0400

    updating

--- Ours-parallel/KPlex_BB_matrix.h
+++ Ours-parallel/KPlex_BB_matrix.h
@@ -638,7 +638,7 @@ if(PART_BRANCH){
 				#pragma omp task firstprivate(td, u, S_end, R_end, level, t_matrix)
 				{
 					swap(matrix, t_matrix);
-					// td->loadData(this);
+					td->loadData(this);
 					// for(ui i=0;i<R_end; i++)if(degree_in_S[SR[i]]>S_end) cout<<"Error"<<degree_in_S[SR[i]]<<" "<<S_end<<endl;
 					// ui pre_best_solution_size = best_solution_size, t_old_S_end = S_end, t_old_R_end = R_end, t_old_removed_edges_n = 0;
 					// if(move_u_to_S_with_prune(u, S_end, R_end, level)) BB_search(S_end, R_end, level+1, false, false, TIME_NOW);
