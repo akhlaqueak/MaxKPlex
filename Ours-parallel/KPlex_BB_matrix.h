47ba52050b7a73923e7d3cdab90d97e6193fce52
akhlaque <akhlaque.ak@gmail.com>
2024-09-25 17:17:49 -0400

    updating

--- Ours-parallel/KPlex_BB_matrix.h
+++ Ours-parallel/KPlex_BB_matrix.h
@@ -154,6 +154,7 @@ public:
 		level_id=new ui[n];
 		neighbors = new ui[n];
 		nonneighbors = new ui[n];
+		best_solution = new ui[n];
 		S2 = new ui[n];
 		copy(src.SR, src.SR+n, SR);
 		copy(src.SR_rid, src.SR_rid+n, SR_rid);
