cb5d1122a3f90f6066b64d885ef24f0424367b1e
akhlaque <akhlaque.ak@gmail.com>
2024-09-28 15:41:28 -0400

    updating

--- Ours-serial/KPlex_BB_matrix.h
+++ Ours-serial/KPlex_BB_matrix.h
@@ -9,7 +9,7 @@
 #define S2RULE
 
 // SR_BRANCHING can take values S_branching, R_branching, SR_branching
-#define SR_BRANCHING S_branching
+#define SR_BRANCHING R_branching
 // if PART_BRANCH is false, then pivot branch gets executed... 
 #define PART_BRANCH (true)
 // #define PART_BRANCH (K<=5&&sparse)
@@ -570,7 +570,7 @@ if(PART_BRANCH){
 // ******************* Adding our branching stuff here... 
 		ui t_R_end=R_end;
 
-		R_end = S_branching(S_end, R_end, level);
+		R_end = SR_BRANCHING(S_end, R_end, level);
 		while(R_end<t_R_end){
 		// branching vertices are now in R_end to t_R_end, and they are already sorted in peelOrder
 			// move branching vertex back to C
