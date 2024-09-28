a5303319973349f6264a1660dbd0d6d06a27e301
akhlaque <akhlaque.ak@gmail.com>
2024-09-28 15:28:59 -0400

    updating

--- Ours-serial/KPlex_BB_matrix.h
+++ Ours-serial/KPlex_BB_matrix.h
@@ -8,8 +8,8 @@
 // pruning switches
 #define S2RULE
 
-// SR_BRANCHING can take values S_Branching, R_Branching, SR_Branching
-#define SR_BRANCHING S_Branching
+// SR_BRANCHING can take values S_branching, R_branching, SR_branching
+#define SR_BRANCHING S_branching
 // if PART_BRANCH is false, then pivot branch gets executed... 
 #define PART_BRANCH (true)
 // #define PART_BRANCH (K<=5&&sparse)
