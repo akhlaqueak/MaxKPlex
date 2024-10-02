7627d594bb9aae6cf0e93641f403e22c7d2e8240
akhlaque <akhlaque.ak@gmail.com>
2024-10-02 19:31:04 -0400

    updating

--- Ours-serial/KPlex_BB_matrix.h
+++ Ours-serial/KPlex_BB_matrix.h
@@ -9,15 +9,15 @@
 #define S2RULE
 
 // SR_BRANCHING can take values S_branching, R_branching, SR_branching
-#define SR_BRANCHING SR_branching
+#define SR_BRANCHING S_branching
 // if PART_BRANCH is false, then pivot branch gets executed... 
-#define PART_BRANCH (true)
-// #define PART_BRANCH (K<=5&&sparse)
+// #define PART_BRANCH (true)
+#define PART_BRANCH (K<=5&&sparse)
 
 
 // Upper bounding switches... 
 // #define SEESAW
-// #define COLORBOUND
+#define COLORBOUND
 // #define PART_BOUND
 
 #define CSIZE (R_end-S_end)
