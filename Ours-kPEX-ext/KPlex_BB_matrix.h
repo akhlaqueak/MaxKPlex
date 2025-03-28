6d8098ad2160a86a9682968dc0a47db38797b718
akhlaque <akhlaque.ak@gmail.com>
2025-03-28 10:34:56 -0400

    updating

--- Ours-kPEX-ext/KPlex_BB_matrix.h
+++ Ours-kPEX-ext/KPlex_BB_matrix.h
@@ -885,7 +885,7 @@ else{ // pivot based branching
 			else UB_l=UB;
 			ui LB_r = best_solution_size + 1 - S_end - UB_l;
 			cout<<S_end<<":"<<R_l<<":"<<R_end<<"...>";
-			cout<<LB_l<<":"<<LB_r<<":"<<UB<<end;
+			cout<<LB_l<<":"<<LB_r<<":"<<UB<<endl;
 			// RR1 on C_r
 			if(!alt_reduction_rules(S_end, R_end, R_l, LB_l, LB_r, right, level)) {ret = false; break;}
 
