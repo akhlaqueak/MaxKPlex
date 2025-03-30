3b067105adf33392f7e3cf9d0fc8ec8297aa0548
akhlaque <akhlaque.ak@gmail.com>
2025-03-30 16:21:34 -0400

    updating

--- kPEX/kPEX/Branch.h
+++ kPEX/kPEX/Branch.h
@@ -442,7 +442,7 @@ public:
         }
 
         for(int u:C){
-            if(u!=sel and non_A[sel][u])
+            if(u!=sel and A[sel][u])
                 B.push_back(u);
         }
         B.push_back(sel);
