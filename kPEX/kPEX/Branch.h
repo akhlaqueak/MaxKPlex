c2156a0aa978c6e7a7d07750f2b679659e5e21a7
akhlaque <akhlaque.ak@gmail.com>
2025-03-31 13:07:00 -0400

    updating

--- kPEX/kPEX/Branch.h
+++ kPEX/kPEX/Branch.h
@@ -114,7 +114,7 @@ public:
 
         // select pivot to generate 2 branches
         int pivot = -1;
-#define BIN_BRANCHING
+// #define BIN_BRANCHING
 // #define PIVOT_BRANCHING
 #ifdef BIN_BRANCHING
         pivot = select_pivot_vertex_with_min_degree(C);
@@ -133,6 +133,7 @@ public:
 #else
         // partition based branching
         auto br_C = branching_set_part(S, C);
+        cout<<br_C.size()<<" ";
         for (int u : br_C)
         {
             auto newS = S, newC=C;
