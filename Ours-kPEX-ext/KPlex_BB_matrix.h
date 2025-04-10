a21957459fa1f6f93cc06dd2dfe4ef470c1ad76e
akhlaque <akhlaque.ak@gmail.com>
2025-04-10 15:08:27 -0400

    updating

--- Ours-kPEX-ext/KPlex_BB_matrix.h
+++ Ours-kPEX-ext/KPlex_BB_matrix.h
@@ -7,7 +7,7 @@
 
 // pruning switches
 #define S2RULE
-#define ALTRB
+// #define ALTRB
 // SR_BRANCHING can take values S_branching, R_branching, SR_branching
 #define SR_BRANCHING S_branching
 // if PART_BRANCH is false, then pivot branch gets executed...
