7128f8d626f71457cf20b17883a0e74e2aa72d48
akhlaque <akhlaque.ak@gmail.com>
2025-08-14 14:11:24 -0400

    updating

---
+++ degree-check/read_bin.cpp
@@ -0,0 +1,44 @@
+#include "Utility.h"
+
+int main(int argc, char *argv[]) {
+		char* fname = argv[1];
+		FILE *f = Utility::open_file(fname, "rb");
+	
+		ui tt, n, m, max_degree=0;
+		fread(&tt, sizeof(int), 1, f);
+		fread(&n, sizeof(int), 1, f);
+		fread(&m, sizeof(int), 1, f);
+	
+		
+		ui *degree = new ui[n];
+		ui* pstart = new ept[n+1];
+		ui* edges = new ui[m];
+		
+		fread(degree, sizeof(int), n, f);
+		pstart[0] = 0;
+		for(ui i = 0;i < n;i ++) {
+			if(degree[i] > 0) {
+				fread(edges+pstart[i], sizeof(int), degree[i], f);
+				
+				// remove self loops and parallel edges
+				ui *buff = edges+pstart[i];
+				sort(buff, buff+degree[i]);
+				ui idx = 0;
+				for(ui j = 0;j < degree[i];j ++) {
+					if(buff[j] >= n) printf("vertex id %u wrong\n", buff[j]);
+					if(buff[j] == i||(j > 0&&buff[j] == buff[j-1])) continue;
+					buff[idx ++] = buff[j];
+				}
+				degree[i] = idx;
+			}
+			
+			pstart[i+1] = pstart[i] + degree[i];
+			max_degree = max(degree[i], max_degree);
+		}
+		// printf("\tn = %, m = %s (undirected), max_degree = %s \n", Utility::integer_to_string(n).c_str(), Utility::integer_to_string(m/2).c_str(), Utility::integer_to_string(max_degree).c_str());
+		cout<<fname<<" n = "<<n<<" m = "<<m<<" d_max = "<<max_degree<<endl;
+		fclose(f);
+		delete[] degree;
+		delete[] pstart;
+		delete[] edges;
+	}
